702f11c5c303e8ca0a40c508cc71508897a42eb0
Andres Segura <asegura@techexchange.in>
2022-03-02 19:45:16 +0000

    initial commit, still need to bring in data from csv

--- Unit2/inventory.py
+++ Unit2/inventory.py
@@ -6,41 +6,41 @@ class Inventory:
         self.electronics = {}
 
     def add_new_item(self, instrument):
-        dictionary = instrument_list(instrument.type)
+        dictionary = self.return_instrument_dict(instrument.category)
         dictionary[instrument.name] = instrument
 
-    def update_quantity(self, type, name, quantity):
-        dictionary = instrument_list(type)
+    def update_quantity(self, category, name, quantity):
+        dictionary = self.return_instrument_dict(category)
         if name not in dictionary:
             raise ValueError("name not in inventory")
         if quantity > dictionary[name].quantity:
             raise ValueError("invalid quantity number")
         dictionary[name].quantity += quantity
 
-    def change_price(self, name, type, new_price):
-        dictionary = instrument_list(type)
-        if name not in list:
+    def change_price(self, name, category, new_price):
+        dictionary = self.return_instrument_dict(category)
+        if name not in dictionary:
             raise ValueError("name not in inventory")
         if new_price < 0 or new_price > 15000:
             raise ValueError("invalid price")
         dictionary[name].price = new_price
 
-    def check_inventory(self, type, name):
-        dictionary = instrument_list(type)
+    def check_inventory(self, category, name):
+        dictionary = self.return_instrument_dict(category)
         if name not in dictionary:
             raise ValueError("name not in inventory")
         return dictionary[name].quantity
 
-    def instrument_list(self, type):
-        if type not in ['guitar', 'bass', 'drums', 'electronics']:
-            raise TypeError("no inventory items of that type")
-        if type == 'guitar':
+    def return_instrument_dict(self, category):
+        if category not in ['guitar', 'bass', 'drums', 'electronics']:
+            raise TypeError("no inventory items of that category")
+        if category == 'guitar':
             return self.guitar
-        elif type == 'bass':
+        elif category == 'bass':
             return self.bass
-        elif type == 'drums':
+        elif category == 'drums':
             return self.drums
-        elif type == 'electronics':
+        elif category == 'electronics':
             return self.electronics
 
 
