b50e6624bf91e391d1853a08fc96d17aaa1a5d68
HiramLpzRdz <hiramlpzrdz@techexchange.in>
2022-03-02 20:04:40 +0000

    Created the item class and the test file for inventory

---
+++ Unit2/item.py
@@ -0,0 +1,8 @@
+class Item:
+    def __init__(self):
+        self.name = {}
+        self.price = {}
+        self.serialNumber = {}
+        self.stock = {}
+        self.description = {}
+        self.category = {}
