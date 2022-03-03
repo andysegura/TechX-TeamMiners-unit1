c3309f5be17dea1ad694ad5de21ced1230fd66fa
HiramLpzRdz <hiramlpzrdz@techexchange.in>
2022-03-03 18:28:03 +0000

    Test file for invetory

--- Unit2/test_inventory.py
+++ Unit2/test_inventory.py
@@ -2,16 +2,19 @@ import unittest
 import inventory
 
 class TestInventory(unittest.TestCase):
-    
 
     #update_quantity
     def test_update_quantity(self):
+        self.assertRaises(ValueError, inventory.update_quantity, 'axe', 1)
     
     #change_price
-    def test_change_price(self)
+    def test_change_price(self):
+        self.assertRaises(ValueError, inventory.change_price, 'axe', 1)
 
     #check_inventory
-    def test_check_inventory(self)
+    def test_check_inventory(self):
+        self.assertRaises(ValueError, inventory.check_inventory, 'axe', 1)
 
     #return_instrument_dic
-    def test_return_instrument_dic(self)
+    def test_return_instrument_dic(self):
+        self.assertRaises(ValueError, inventory.return_instrument, 'axe', 1)
