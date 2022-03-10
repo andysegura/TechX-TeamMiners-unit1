e3feaf84d9339f035fcfc2b66f329bc0ddac2fac
ryanjwin <ryanjwin2020@gmail.com>
2022-03-09 23:34:44 -0700

    added more test

--- Unit2/test_item.py
+++ Unit2/test_item.py
@@ -6,11 +6,17 @@ class TestItem(unittest.TestCase):
     def setUp(self):
         self.item1 = Item('guitar','Gibson Les Paul Standard',np.float64(500.00) ,'10001a',np.int64(5),'abc')
         self.item2 = Item('bass','Ibanez Acoustic Electric',np.float64(300.10) ,'10100a', np.int64(2),'abc')
+
     def test00_init(self):
         # make sure that everything was correct
         self.assertEqual(self.item1.get_name(), 'Gibson Les Paul Standard')
         self.assertEqual(self.item1.get_price(), 500.00)
-
+        self.assertRaises(TypeError, Item, 'guitar', 'guitar','Gibson Les Paul Standard', np.float64(500.00) ,'10001a',np.int64(5),'abc')
+        self.assertRaises(TypeError, Item, 'guitar', 'guitar','Gibson Les Paul Standard', '500' ,'10001a','5','abc')
+        self.assertRaises(TypeError, Item, 'bass','Ibanez Acoustic Electric',np.float64(300.10) ,'10100a', np.int64(2),55)
+        self.assertRaises(ValueError, Item, 'bass','Ibanez Acoustic Electric',np.float64(-200) ,'10100a', np.int64(2),'abc')
+        self.assertRaises(ValueError, Item, 'bass','Ibanez Acoustic Electric',np.float64(200) ,'10100a', np.int64(-2),'abc')
+        
     def test01_update_stock(self):
         self.item1.update_stock(2)
         self.item2.update_stock(-1)
@@ -20,5 +26,6 @@ class TestItem(unittest.TestCase):
 
     def test03_set_price(self):
         self.assertRaises(TypeError, self.item2.set_price, "string")
+        self.assertRaises(ValueError, self.item1.set_price, np.float64(-200))
         self.item2.set_price(np.float64(200))
         self.assertEqual(self.item2.get_price(), 200)
