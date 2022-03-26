70dcbd9075a6404deb4361a22d1a70f81b46d1e8
Andres Segura <asegura@techexchange.in>
2022-03-26 20:35:43 +0000

    basic app.py outline for unit 3

--- Unit3/app.py
+++ Unit3/app.py
@@ -12,14 +12,32 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'database'
 
 # URI of database
-app.config['MONGO_URI'] = "<replace_with_real_mongodb_url>"
+app.config['MONGO_URI'] = "<mongo_uri_here>"
 
 #Initialize PyMongo
 mongo = PyMongo(app)
 
-# -- Routes section --
-# INDEX Route
+# Comment out this create_collection method after you run the app for the first time
+# mongo.db.create_collection('library')
+
+## -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/<department>')
+def department_view(department):
+    collection = mongo.db.music_store
+    instruments = collection.find({'department.html': department})
+    return render_template('department.html', instruments = instruments)
+
+@app.route('/<instrument_name>')
+def instrument_view(instrument_name):
+    collection = mongo.db.music_store
+    instrument = collection.find({'name': instrument_name})
+    return render_template('instrument.html', instrument = instrument)
+
+@app.route('/shopping_cart')
+def shopping_cart_view():
+    return render_template('shopping_cart.html')
