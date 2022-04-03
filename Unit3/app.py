389794ac4eb83249379c8b5e44780dbe82ef8044
ryanjwin <ryanjwin2020@gmail.com>
2022-04-02 23:24:54 -0600

    added shopping cart in session

--- Unit3/app.py
+++ Unit3/app.py
@@ -2,11 +2,13 @@
 # -- Import section --
 from flask import Flask
 from flask import render_template
-from flask import request, redirect
+from flask import request, redirect, session
 from flask_pymongo import PyMongo
+from model import Shopping_Cart
 import certifi
 import pymongo
-
+import secrets
+import json
 # -- Initialization section --
 app = Flask(__name__)
 
@@ -23,18 +25,19 @@ db = client.miners_music
 inv = db.inventory
 # Comment out this create_collection method after you run the app for the first time
 # mongo.db.create_collection('library')
-
+app.secret_key = secrets.token_urlsafe(16)
 ## -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    session['cart'] = Shopping_Cart().__dict__
+    return render_template('index.html', shopping_cart=session['cart'])
 
 @app.route('/<department>')
 def department_view(department):
     collection = mongo.db.inventory
     instruments = collection.find({'category': department})
-    return render_template('department.html', instruments = instruments)
+    return render_template('department.html', instruments = instruments, shopping_cart=session['cart'])
 
 @app.route('/<department>/<model_number>')
 def instrument_view(department, model_number):
@@ -44,8 +47,8 @@ def instrument_view(department, model_number):
         return 'invalid page id'
     if instrument['category'] != department:
         return f"{model_number} not in this department"
-    return render_template('instrument.html', instrument = instrument)
+    return render_template('instrument.html', instrument = instrument, shopping_cart=session['cart'])
 
 @app.route('/shopping_cart')
 def shopping_cart_view():
-    return render_template('shopping_cart.html')
+    return render_template('shopping_cart.html', shopping_cart=session['cart'])
