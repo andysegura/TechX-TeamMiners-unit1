e6d52667b4956ba636bf7d38b09b03ceacce7a5b
ryanjwin <ryanjwin2020@gmail.com>
2022-04-03 17:48:26 -0600

    added cart in cookies functionality

--- Unit3/app.py
+++ Unit3/app.py
@@ -1,10 +1,13 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
+from operator import mod
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 from flask import request, redirect, session
 from flask_pymongo import PyMongo
-from model import Shopping_Cart
+from flask import make_response
+from model import Shopping_Cart, can_add
+from flask_session import Session
 import certifi
 import pymongo
 import secrets
@@ -25,30 +28,57 @@ db = client.miners_music
 inv = db.inventory
 # Comment out this create_collection method after you run the app for the first time
 # mongo.db.create_collection('library')
-app.secret_key = secrets.token_urlsafe(16)
+#db.create_collection('sessions')
 ## -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    session['cart'] = Shopping_Cart().__dict__
-    return render_template('index.html', shopping_cart=session['cart'])
+    resp = make_response(render_template('index.html', count = request.cookies.get('count')))
+    if 'cart' not in request.cookies:
+        resp.set_cookie('cart', json.dumps({}))
+        resp.set_cookie('count', '0')
+        resp.set_cookie('total', '0')
+    return resp
 
 @app.route('/<department>')
 def department_view(department):
-    collection = mongo.db.inventory
-    instruments = collection.find({'category': department})
-    return render_template('department.html', instruments = instruments, shopping_cart=session['cart'])
+    instruments = inv.find({'category': department})
+    return render_template('department.html', instruments = instruments, count = request.cookies.get('count'))
 
-@app.route('/<department>/<model_number>')
+
+@app.route('/<department>/<model_number>', methods=['GET', 'POST'])
 def instrument_view(department, model_number):
-    collection = db.inventory
-    instrument = collection.find_one({'model_number': str(model_number)})
+    instrument = inv.find_one({'model_number': str(model_number)})
     if not instrument:
         return 'invalid page id'
     if instrument['category'] != department:
         return f"{model_number} not in this department"
-    return render_template('instrument.html', instrument = instrument, shopping_cart=session['cart'])
+    return render_template('instrument.html', instrument = instrument, count = request.cookies.get('count'))
+
 
 @app.route('/shopping_cart')
 def shopping_cart_view():
-    return render_template('shopping_cart.html', shopping_cart=session['cart'])
+    return render_template('shopping_cart.html', count = request.cookies.get('count'))
+
+@app.route('/create_cart')
+def create_cart():
+    resp = make_response(redirect(url_for('index')))
+    resp.set_cookie('cart', json.dumps({}))
+    resp.set_cookie('count', '0')
+    resp.set_cookie('total', '0')
+    return 'created cookies'
+
+@app.route('/add_to_cart/<department>/<model_number>',  methods=['GET', 'POST'])
+def add_to_cart(department, model_number):
+    cart = json.loads(request.cookies.get('cart'))
+    count = int(request.cookies.get('count'))
+    if can_add(cart, model_number):
+        if model_number in cart:
+            cart[model_number] += 1
+        else:
+            cart[model_number] = 1
+    resp = make_response(redirect(url_for('instrument_view', department=department, model_number=model_number)))
+    resp.set_cookie('cart', json.dumps(cart))
+    resp.set_cookie('count', str(count + 1))
+    return resp
+    return redirect(url_for('instrument_view', department=department, model_number=model_number))
--- Unit3/model.py
+++ Unit3/model.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from pyexpat import model
 import pymongo
 import certifi
 # create database
@@ -86,3 +87,27 @@ class Shopping_Cart:
         else:
             raise ValueError('The product is not in the cart!')
         return True
+
+
+def can_add(shopping_cart, model_number):
+    """
+        Adds a new item of the model number to the cart.  If there is still stock available
+
+        Parameters
+        ----------
+
+        model_number : str
+            A string representing the model number of the item to be added to the cart
+
+        Returns
+        -------
+
+        bool
+            Returns True if item was successfuly added
+    """
+    item = inv.find_one({'model_number': model_number})
+    if not item:
+        raise ValueError('We do not have this item in inventory')
+    elif (model_number in shopping_cart and item['stock'] < shopping_cart[model_number] + 1) or item['stock'] < 1:
+        raise ValueError('There is not enough stock of this item to complete the transaction')
+    return True
